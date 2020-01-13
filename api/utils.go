5b01a973d7facf9e67b5abe7ffe4b4bc1958e41e
SOBIT <staking@outofbits.com>
2020-01-13 05:07:40 +0100

    Adds utils for sorting and filtering leader assignment.

--- api/utils.go
+++ api/utils.go
@@ -1,6 +1,10 @@
 package api
 
-import "net/url"
+import (
+    "net/url"
+    "sort"
+    "time"
+)
 
 // combines the base URL with the given path. if the given
 // path is not valid, then an error will be returned.
@@ -10,4 +14,23 @@ func combine(base *url.URL, path string) (*url.URL, error) {
         return base.ResolveReference(ref), nil
     }
     return nil, err
+}
+
+func SortLeaderLogsByScheduleTime(leaderAssignments []LeaderAssignment) []LeaderAssignment {
+    sortedAssignment := leaderAssignments[:]
+    sort.Slice(sortedAssignment, func(i, j int) bool {
+        return leaderAssignments[i].ScheduleTime.Before(leaderAssignments[j].ScheduleTime)
+    })
+    return sortedAssignment
+}
+
+func FilterLeaderLogsBefore(before time.Time, leaderAssignments []LeaderAssignment) []LeaderAssignment {
+    var filteredAssignments []LeaderAssignment
+    for i := range leaderAssignments {
+        currentAssignment := leaderAssignments[i]
+        if currentAssignment.ScheduleTime.After(before) {
+            filteredAssignments = append(filteredAssignments, currentAssignment)
+        }
+    }
+    return filteredAssignments
 }
